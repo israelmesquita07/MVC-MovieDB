47660e592b3251fb73d0e751de904be594cefc32
Israel Mesquita <israelmesquita07@gmail.com>
2019-10-16 02:11:29 -0300

    first commit

---
+++ PagMovies/DetailsMovieViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailsMovieViewController.swift
+//  PagMovies
+//
+//  Created by Israel on 16/10/19.
+//  Copyright © 2019 israel3D. All rights reserved.
+//
+
+import UIKit
+
+class DetailsMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
